8a86df0e4f614db1140419abae3e8e63cc94963a
Gabriel Cuenca Moncín <gabriel@redradix.com>
2020-09-11 16:58:55 +0200

    :art: :hammer: Refactor

--- index.js
+++ index.js
@@ -2,20 +2,21 @@
 // 'bla' => { name: 'bla' }
 // 'tag = triki' => { tag: triki }
 // 'tag in (triki,traun)' => { tag: { $in: ['triki', 'traun'] } }
+// 'name not bla' => { name: { $not: 'bla' } }
 
 const splitStringWithAnIn = (string) => {
   const [key, subkey, value] = string.split(' ')
 
   return {
     [key]: {
-      [`\$${ subkey }`]: value.replace(/[\(\)]/g, '').split(','),
+      [`\$${subkey}`]: value.replace(/[\(\)]/g, '').split(','),
     }
   }
 }
 
 // string => obj
 const enigma = string => {
-  const elements = string.split('=')
+  const elements = string.split(' ')
 
   if (string.includes('in')) {
     return splitStringWithAnIn(string)
@@ -23,11 +24,19 @@ const enigma = string => {
 
   if (elements.length === 1) {
     return { name: elements[0] }
-  } else {
-    const [key, value] = elements
-    return { [key.trim()]: value.trim() }
+  }
+
+  if (elements.length === 3) {
+    const [key, operation, value] = elements
+
+    switch (operation) {
+      case 'in': return splitStringWithAnIn(string)
+      case '=': return { [key.trim()]: value.trim() }
+    }
   }
 }
+
+console.log(enigma('name not bla'))
 
 module.exports = {
   enigma,
