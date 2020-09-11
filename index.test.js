08fefbac83210565a05de301de7d012147650548
Álvaro Cuesta <alvaro@redradix.com>
2020-09-11 15:54:54 +0200

    Comment SAMPLES

--- index.test.js
+++ index.test.js
@@ -1,4 +1,6 @@
 describe('eNiGmA', () => {
+  /*
+
   const SAMPLES = [
     { input: 1, expected: 2 }
   ]
@@ -8,6 +10,8 @@ describe('eNiGmA', () => {
       expect(1).toBe(2)
     })
   })
+
+  */
 
   it('Returns first part of the string as the key and the second part of it as the value of the object ', () => {
     expect(eNiGmA('name = foo')).toEqual({ name: 'foo' })
