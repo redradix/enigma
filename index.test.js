fbb10fcebc5ba5ca627ed8dfa38d407092d8aebc
Álvaro Cuesta <alvaro@redradix.com>
2020-09-11 16:17:49 +0200

    Refactor tests to reuse SAMPLES

--- index.test.js
+++ index.test.js
@@ -1,28 +1,21 @@
 const { eNiGmA } = require('./index')
 
 describe('eNiGmA', () => {
-  /*
+  describe('Returns first part of the string as the key and the second part of it as the value of the object ', () => {
+    const SAMPLES = [
+      { input: 'name = foo', expected: { name: 'foo' } },
+      { input: 'name = bar', expected: { name: 'bar' } },
+      { input: 'name = lol', expected: { name: 'lol' } },
+    ]
 
-  const SAMPLES = [
-    { input: 1, expected: 2 }
-  ]
-
-  SAMPLES.forEach(sample => {
-    it(`expects ${sample.input} to return ${sample.expected}`, () => {
-      expect(1).toBe(2)
+    SAMPLES.forEach(sample => {
+      it(`expects ${sample.input} to return ${JSON.stringify(sample.expected)}`, () => {
+        expect(eNiGmA(sample.input)).toEqual(sample.expected)
+      })
     })
-  })
-
-  */
-
-  it('Returns first part of the string as the key and the second part of it as the value of the object ', () => {
-    expect(eNiGmA('name = foo')).toEqual({ name: 'foo' })
-    expect(eNiGmA('name = bar')).toEqual({ name: 'bar' })
-    expect(eNiGmA('name = lol')).toEqual({ name: 'lol' })
   })
 })
 
 
 
-// 'name = foo' => { name: 'foo' }
 // 'bla' => { name: 'bla' }
